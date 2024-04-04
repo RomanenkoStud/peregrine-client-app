97cca03cb63f77debfc017a8449331f90c874cc4
RomanenkoStud <pavlo.romanenko@nure.ua>
2024-04-04 20:01:05 +0300

    PR Add logs

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -8,3 +8,5 @@ declare global {
 export const db = globalThis.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+
+console.log(process.env.DATABASE_URL)
